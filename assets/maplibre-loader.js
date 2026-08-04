991ef707fb99014fe537695b467df7aeeffa0926
daniels200518-wq <daniels200518@gmail.com>
2026-08-04 08:32:27 -0400

    Delete assets directory

--- assets/maplibre-loader.js
+++
@@ -1,8 +0,0 @@
-import("https://unpkg.com/maplibre-gl@6.0.0/dist/maplibre-gl.mjs")
-  .then(module => {
-    window.maplibregl = module;
-    window.dispatchEvent(new Event("maplibre-ready"));
-  })
-  .catch(() => {
-    window.dispatchEvent(new Event("maplibre-error"));
-  });
